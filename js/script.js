ddd54dcd535ddf8cb46da385953842f273bbfd42
Mateusz Kubiak <mateusz.kubiak.1991@gmail.com>
2020-07-06 23:29:17 +0200

    adding new functions in JS flie

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
-const baseValue = document.querySelector('.main__section--baseInput');
-const targetCurrency = document.querySelector('.main__section--formSelectTarget');
-const formElement = document.querySelector('.main__section--form');
-let resultValue = document.querySelector('.main__section--resultInput');
+{
+    const init = () => {
+        const formElement = document.querySelector('.main__section--form');
 
-formElement.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    let CHF = 4.1919;
-    let EUR = 4.6416;
-    let USD = 3.9697;
-    let GBP = 4.9250;
-    let valuePLN = baseValue.value;
-
-    switch (targetCurrency.value) {
-        case "CHF":
-            resultValue.innerText = (valuePLN / CHF).toFixed(4);
-            break;
-        case "EUR":
-            resultValue.innerText = (valuePLN / EUR).toFixed(4);
-            break;
-        case "USD":
-            resultValue.innerText = (valuePLN / USD).toFixed(4);
-            break;
-        case "GBP":
-            resultValue.innerText = (valuePLN / GBP).toFixed(4);
-            break;
-        default:
-            resultValue.innerText = "coś poszło nie tak..."
+        formElement.addEventListener('submit', (event) => {
+            event.preventDefault();
+    
+            const baseValue = document.querySelector('.main__section--baseInput').value;
+            const targetCurrency = document.querySelector('.main__section--formSelectTarget').value;
+            let resultValue = document.querySelector('.main__section--resultSpan');
+            const CHF = 4.1919;
+            const EUR = 4.6416;
+            const USD = 3.9697;
+            const GBP = 4.9250;
+    
+            switch (targetCurrency) {
+                case "CHF":
+                    resultValue.innerText = (baseValue / CHF).toFixed(4);
+                    break;
+                case "EUR":
+                    resultValue.innerText = (baseValue / EUR).toFixed(4);
+                    break;
+                case "USD":
+                    resultValue.innerText = (baseValue / USD).toFixed(4);
+                    break;
+                case "GBP":
+                    resultValue.innerText = (baseValue / GBP).toFixed(4);
+                    break;
+                default:
+                    resultValue.innerText = "coś poszło nie tak..."
+            }
+        });
     }
-});
+    init();
+}
