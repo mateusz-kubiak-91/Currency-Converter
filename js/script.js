c20bba380886e678729853cf925c4eaf009627a0
Mateusz Kubiak <mateusz.kubiak.1991@gmail.com>
2020-07-07 21:41:52 +0200

    add background img and function calculator

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
 {
+    const calculator = () => {
+        const baseValue = document.querySelector('.main__section--baseInput').value;
+        const targetCurrency = document.querySelector('.main__section--formSelectTarget').value;
+        let resultValue = document.querySelector('.main__section--resultSpan');
+        const CHF = 4.1919;
+        const EUR = 4.6416;
+        const USD = 3.9697;
+        const GBP = 4.9250;
+
+        switch (targetCurrency) {
+            case "CHF":
+                resultValue.innerText = (baseValue / CHF).toFixed(4);
+                break;
+            case "EUR":
+                resultValue.innerText = (baseValue / EUR).toFixed(4);
+                break;
+            case "USD":
+                resultValue.innerText = (baseValue / USD).toFixed(4);
+                break;
+            case "GBP":
+                resultValue.innerText = (baseValue / GBP).toFixed(4);
+                break;
+            default:
+                resultValue.innerText = "coś poszło nie tak..."
+        }
+    }
+
     const init = () => {
         const formElement = document.querySelector('.main__section--form');
 
-        formElement.addEventListener('submit', (event) => {
+        formElement.addEventListener('submit', () => {
             event.preventDefault();
-    
-            const baseValue = document.querySelector('.main__section--baseInput').value;
-            const targetCurrency = document.querySelector('.main__section--formSelectTarget').value;
-            let resultValue = document.querySelector('.main__section--resultSpan');
-            const CHF = 4.1919;
-            const EUR = 4.6416;
-            const USD = 3.9697;
-            const GBP = 4.9250;
-    
-            switch (targetCurrency) {
-                case "CHF":
-                    resultValue.innerText = (baseValue / CHF).toFixed(4);
-                    break;
-                case "EUR":
-                    resultValue.innerText = (baseValue / EUR).toFixed(4);
-                    break;
-                case "USD":
-                    resultValue.innerText = (baseValue / USD).toFixed(4);
-                    break;
-                case "GBP":
-                    resultValue.innerText = (baseValue / GBP).toFixed(4);
-                    break;
-                default:
-                    resultValue.innerText = "coś poszło nie tak..."
-            }
+
+            calculator();
         });
     }
     init();
